f9fa780ad85ce463cd70e33d8d21fa5ed6557db4
User <User@DESKTOP-44DT77C>
2026-02-02 09:25:25 +0500

    Login qismini ishlab chiqdim, bunda massiv lardan foydalandim.

--- LogIn/Program.cs
+++ LogIn/Program.cs
@@ -1,1 +1,48 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+
+string[] logIn = {"doston"};
+string[] password = {"doston2001$"};
+
+belgi:
+Console.WriteLine("Welcome my Project");
+Console.WriteLine("1. LogIn");
+Console.WriteLine("2. SignUp");
+
+Console.Write("Enter your command: ");
+int command = Convert.ToInt32(Console.ReadLine());
+
+
+if (command == 1)
+{
+    Console.WriteLine("===== LogIn ======");
+    Console.Write("Enter your logIn: ");
+    string logInInput = Console.ReadLine();
+    Console.Write("Enter your password: ");
+    string passwordInput = Console.ReadLine();
+    bool isAuthenticated = false;
+    for (int i = 0; i < logIn.Length; i++)
+    {
+        if (logInInput == logIn[i] && passwordInput == password[i])
+        {
+            isAuthenticated = true;
+            break;
+        }
+    }
+    if (isAuthenticated)
+    {
+        Console.Clear();
+        Console.WriteLine("You have successfully logged in!");
+    }
+    else
+    {
+        Console.Clear();
+        Console.WriteLine("Invalid logIn or password.");
+    }
+}
+
+
+
+
+goto belgi;
+
+
